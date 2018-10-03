9863ca5e19ed4af9a48282c52e2f6f1ac5468ce7
Luke Phillips <lcphillips8086@gmail.com>
2018-10-03 09:02:54 -0700

    Turn on -Wall

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xc16-gcc -mcpu=33EP32GP502 -T p33EP32GP502.gld -o test.elf test.c ecan.c
+xc16-gcc -Wall -mcpu=33EP32GP502 -T p33EP32GP502.gld -o test.elf ecan.c test.c
 xc16-bin2hex test.elf
