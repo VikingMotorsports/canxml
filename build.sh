4dae4235d5af0149a73f16bb60c7573bab674a15
Luke Phillips <lcphillips8086@gmail.com>
2019-01-10 13:17:04 -0800

    Update build scripts.

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xc16-gcc -g -Wall -mcpu=33EP32GP502 -T p33EP32GP502.gld -o test.elf ecan.c test.c
+xc16-gcc -g -Wall -mcpu=33EP32GP502 -T p33EP32GP502.gld -o test.elf ecan.c oscconfig.c test.c
 xc16-bin2hex test.elf
