ef93976b9ee83f02cd78558aeb0ff8eba43c4e7a
Luke Phillips <lcphillips8086@gmail.com>
2019-02-26 13:13:01 -0800

    Moved a bunch of files to separate the driver-generation code from more general utilities and testing code.

--- build_io_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-xc16-gcc -g -Wall -mcpu=33EP32GP502 -T p33EP32GP502.gld -o test_io.elf oscconfig.c test_io.c
-xc16-bin2hex test_io.elf
--- defs.h
+++
@@ -1,6 +0,0 @@
-#ifndef DEFS_H
-#define DEFS_H
-
-#define FCY 30000000
-
-#endif // DEFS_H
